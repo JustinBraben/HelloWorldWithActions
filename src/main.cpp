eeb0c1453b5b6c57f6a9ec55ed6d4a1629295033
Justin Braben <jbraben@dmsiworks.com>
2023-04-26 17:08:46 -0600

    Working copy

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,49 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
+#include <filesystem>
+
 
 int main() {
-	std::cout << "Hello world\n";
-	return 0;
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Hello, World!");
+
+    // Get the path to the executable
+    std::filesystem::path execPath = std::filesystem::current_path();
+
+    // Construct the correct path to the font file
+    std::filesystem::path fontPath = execPath / ".." / ".." / "src" / "assets" / "arial.ttf";
+
+    // Load a font
+    sf::Font font;
+    if (!font.loadFromFile(fontPath.string())) {
+        // Error handling (e.g., use the default font or exit)
+        return 1;
+    }
+
+    // Set up the "Hello, World!" text
+    sf::Text text("Hello, World!", font, 24);
+    text.setFillColor(sf::Color::White);
+    text.setPosition(300, 280);
+
+    // Main loop
+    while (window.isOpen()) {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Clear screen
+        window.clear(sf::Color::Black);
+
+        // Draw the text
+        window.draw(text);
+
+        // Update the window
+        window.display();
+    }
+
+    return 0;
 }
